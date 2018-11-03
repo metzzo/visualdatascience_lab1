7832840c2036aee510ae161cbcd9f60c06c0fed6
Robert Fischer <robert@programming-with-design.at>
2018-11-03 18:50:53 +0100

    add statistical correlations check

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import pickle
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
+
 
 
 def load_dataset(path="../../nutritions.csv"):
@@ -116,6 +116,25 @@ def visual_clustering_by_energy(averages):
     plt.show()
 
 
+def find_statistical_correlations(dataset):
+    nutritions = ["Water_(g)","Energ_Kcal","Protein_(g)","Lipid_Tot_(g)","Ash_(g)","Carbohydrt_(g)","Fiber_TD_(g)","Sugar_Tot_(g)","Calcium_(mg)","Iron_(mg)","Magnesium_(mg)","Phosphorus_(mg)","Potassium_(mg)","Sodium_(mg)","Zinc_(mg)","Copper_mg)","Manganese_(mg)","Selenium_(µg)","Vit_C_(mg)","Thiamin_(mg)","Riboflavin_(mg)","Niacin_(mg)","Panto_Acid_mg)","Vit_B6_(mg)","Folate_Tot_(µg)","Folic_Acid_(µg)","Food_Folate_(µg)","Folate_DFE_(µg)","Choline_Tot_ (mg)","Vit_B12_(µg)","Vit_A_IU","Vit_A_RAE","Retinol_(µg)","Alpha_Carot_(µg)","Beta_Carot_(µg)","Beta_Crypt_(µg)","Lycopene_(µg)","Lut+Zea_ (µg)","Vit_E_(mg)","Vit_D_µg","Vit_D_IU","Vit_K_(µg)","FA_Sat_(g)","FA_Mono_(g)","FA_Poly_(g)","Cholestrl_(mg)"]
+    correlations = []
+    already_done = set()
+    for n1 in nutritions:
+        for n2 in nutritions:
+            if n1 != n2 and n1+n2 not in already_done and n2+n1 not in already_done:
+                corr = np.corrcoef(dataset[n1], dataset[n2])[0, 1]
+                correlations.append({
+                    "corr": corr,
+                    "n1": n1,
+                    "n2": n2
+                })
+                already_done.add(n1+n2)
+                already_done.add(n2+n1)
+    correlations = sorted(correlations, key=lambda x: abs(x['corr']))
+    print(*correlations[-4:-1])
+
+
 ds = load_dataset()
 try:
     print("Load existing dataset")
@@ -136,5 +155,6 @@ except:
 # filter out categories with only 1 entry
 averages = averages[averages[2] > 1]
 
-statistical_clustering_by_energy(averages=averages)
-visual_clustering_by_energy(averages=averages)
+#statistical_clustering_by_energy(averages=averages)
+#visual_clustering_by_energy(averages=averages)
+find_statistical_correlations(dataset=ds)
